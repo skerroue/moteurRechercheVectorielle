42b63c26522aede0fb6471163b8c05a923d520a5
Vincent <Vincent@LAPTOP-AUCL8G5B>
2019-01-29 11:47:41 +0100

    Coucou

--- src/searchEngine/Main.java
+++ src/searchEngine/Main.java
@@ -1,7 +1,6 @@
 package searchEngine;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class Main {
@@ -14,21 +13,10 @@ public class Main {
 		 *  - classement des doc par proximite avec la requete
 		 */
 		
-		TreeMap<String, Keyword> keywords = Index.loadVocabulary();
-		TreeMap<Integer, Document> documents = Index.loadDocuments();
+		TreeMap<String, Keyword> vocabulary = Index.loadVocabulary();
 		
-		Document doc = documents.get(0);
-		List<String> wordsFromDoc = Arrays.asList(doc.getText().split("[ .,;:\"()!?]+"));
-		for (String word : wordsFromDoc) {
-			if (keywords.keySet().contains(word)) {
-				Keyword keyword = keywords.get(word);
-				keyword.add1Occur(0, keyword.get1Occur(0) + 1);
-			}
-		}
-		
-		for (Keyword keyword : keywords.values()) {
-			if (keyword.get1Occur(0) != null)
-				System.out.println(keyword.getTerm() + " " + keyword.get1Occur(0));
+		for (Entry<String, Keyword> string : vocabulary.entrySet()) {
+			System.out.println(string);
 		}
 		
 	}
